af6b41b329e095b97c87473150b3c62539876734
Izabelle wilding <izabellewilding@gmail.com>
2023-12-27 13:03:22 +0000

    redesign 2

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -24,19 +24,30 @@ const LandingPage = React.forwardRef((props, ref) => {
       <div className=" w-full h-full absolute bottom-0 right-0 top-0 left-0 flex">
         <Splashes />
         <div className="relative h-full w-full mx-auto items-center justify-center flex flex-col lg:w-6/12 px-4 ml-auto mr-auto text-center">
-          <div className="inline-block">
-            <h1 className="relative DM-sans text-4xl md:text-6xl text-darkPrimary uppercase leading-none md:mb-3">
-              Izabelle Wilding
+          <div style={{ maxWidth: "580px" }} className="inline-block">
+            <h1 className="mb-2 relative DM-sans text-3xl md:text-6xl text-darkPrimary leading-none md:mb-3 whitespace-nowrap">
+              Hi, I'm <span className="text-teal-100">Izabelle Wilding</span>
             </h1>
-            <p className="landing-subheading quicksand quicksand-letter-spacing text-md md:text-2xl text-lightPrimary uppercase mb-5 md:mb-0 ">
-              Creative Frontend Developer
+            <h1 className="mb-3 relative DM-sans text-3xl md:text-6xl text-darkPrimary leading-none md:mb-3 whitespace-nowrap">
+              Frontend Developer
+            </h1>
+            {/* <p className="landing-subheading quicksand quicksand-letter-spacing text-md md:text-4xl text-lightPrimary mb-5 md:mb-3 ">
+              Frontend Developer
+            </p> */}
+            <p
+              style={{ color: "#37848bde" }}
+              className=" px-12 landing-subheading quicksand quicksand-letter-spacing text-xs md:text-base text-teal-900 mb-5 md:mb-0 tracking-normal m-auto"
+            >
+              I am passionate about crafting visually stunning Web and Mobile
+              applications that prioritize seamless user experiences, robust
+              scalability, and optimal performance.
             </p>
           </div>
           <ScrollButton
             linkto="projects"
             content={
-              <div className="flex justify-center md:mt-12">
-                {<Arrow className="h-12 bounce" />}
+              <div className="flex justify-center mt-8 md:mt-12">
+                {<Arrow className=" h-8 md:h-12 bounce" />}
               </div>
             }
           />
