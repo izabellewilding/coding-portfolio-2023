03d7ad58af0cdf7418c0da3e3701c5fd282845b3
Izabelle wilding <izabellewilding@gmail.com>
2024-01-11 21:01:06 +0000

    Redesign landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,6 +5,9 @@ import Arrow from "../assets/arrow4-black.svg";
 import { ScrollButton } from "./Buttons";
 // import SocialIcons from "../components/SocialIcons.tsx";
 // import SEO from "./seo"
+import Image from "next/image";
+import { outfit } from "../app/fonts";
+import DownArrow from "../assets/down-arrow2-svgrepo-com.svg";
 
 const LandingPage = React.forwardRef((props, ref) => {
   return (
@@ -14,30 +17,40 @@ const LandingPage = React.forwardRef((props, ref) => {
       className=" w-full h-screen items-center justify-center"
       style={{ position: "sticky", top: "0" }}
     >
+      <Image
+        alt="hero image"
+        src="/assets/richard-horvath-_nWaeTF6qo0-unsplash.jpg"
+        layout="fill"
+        objectFit="cover"
+      />
       <Logo className="absolute top-0 left-0 h-12 z-10 m-4" />
-      <div className="gradient relative w-full h-screen"> </div>
-      <div className=" absolute opacity-25 bottom-0 right-0 top-0 left-0" />
+      <div className="relative w-full h-screen" />
+      <div className="absolute opacity-25 bottom-0 right-0 top-0 left-0" />
       <div className=" w-full h-full absolute bottom-0 right-0 top-0 left-0 flex">
-        <Splashes />
-        <div className="relative h-full w-full mx-auto items-center justify-center flex flex-col lg:w-6/12 px-4 ml-auto mr-auto text-center">
-          <div style={{ maxWidth: "580px" }} className="inline-block">
-            <h1 className="mb-2 relative DM-sans text-3xl md:text-6xl text-darkPrimary leading-none md:mb-3 whitespace-nowrap">
-              <span className=""> Izabelle Wilding</span>
-            </h1>
-            <h1 className="mb-3 text-teal-100 relative DM-sans text-3xl md:text-6xl y leading-none md:mb-3 whitespace-nowrap">
-              Frontend Developer
-            </h1>
-          </div>
-          <ScrollButton
-            linkto="projects"
-            content={
-              <div className="flex justify-center mt-8 md:mt-12">
-                {<Arrow className=" h-8 md:h-12 bounce" />}
-              </div>
-            }
-          />
+        {/* <Splashes /> */}
+        <div className="ml-6 relative justify-center h-full w-full flex flex-col px-4 ">
+          {/* <div className="inline-block"> */}
+          <h1
+            style={{ color: "#b1a8ea", fontSize: "100px" }}
+            className="mb-2 uppercase relative text-3xl leading-none md:mb-3"
+          >
+            Izabelle Wilding
+          </h1>
+          <h1
+            className={`mb-3 relative text-2xl md:text-3xl leading-none md:mb-3 whitespace-nowrap ${outfit.className}`}
+          >
+            Software Engineer - UX Specialist -
+          </h1>
         </div>
       </div>
+      <ScrollButton
+        linkto="projects"
+        content={
+          <div className="flex absolute justify-center mt-8 md:mt-12 bottom-10 left-0 right-0 ">
+            <DownArrow />
+          </div>
+        }
+      />
     </main>
   );
 });
